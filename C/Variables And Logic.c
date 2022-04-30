3fdf8d79b52f6a7d7ae68e7a21296d7ffa9ee01c
Korakoe <56580073+korakoe@users.noreply.github.com>
2022-04-30 17:56:36 +0800

    Logic And Variables in C

---
+++ C/Variables And Logic.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+
+	// variables and such
+	int number1;
+	int number2;
+
+	// get values
+	printf("Enter Number 1: ");
+	scanf_s("%d", &number1);
+	printf("Enter Number 2: ");
+	scanf_s("%d", &number2);
+
+	// add values together
+	int sum = number1 + number2;
+
+	// print result
+	printf("Sum = %d \n", sum );
+	
+	// check if lower than 10
+	if (sum < 10) {
+		printf("sum is lower than 10");
+	}
+	else {
+		printf("sum is higher than 10");
+	}
+	
+}
