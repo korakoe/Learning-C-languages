463db30ae77bc2b20dfd4f1ff56f5aa94a6b8e59
Korakoe <56580073+korakoe@users.noreply.github.com>
2022-05-01 15:41:31 +0800

    start a small text adventure game in c++
    also add float support to C++
    float support kinda breaks the C version for some reason

---
+++ C++/Text Adventure.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "-----|| This is a small text adventure game written in C++ ||----- \nThis is just so I can learn file saving, if/else statements and logic \n";
+	cout << "and just some general stuff, there will be a C port aswell";
+
+	return 0;
+}
--- C++/Variables And Logic.cpp
+++ C++/Variables And Logic.cpp
@@ -6,13 +6,13 @@ using namespace std;
 // for the most part this is the same if not simpler than base C
 
 int main() {
-	int num1, num2;
+	float num1, num2;
 	cout << "Enter number one: ";
 	cin >> num1;
 	cout << "Enter number two: ";
 	cin >> num2;
 
-	int sum = num1 + num2;
+	float sum = num1 + num2;
 	cout << "Sum is " << to_string(sum) << "\n";
 
 	if (sum < 10) {
@@ -21,4 +21,5 @@ int main() {
 	else {
 		cout << "Greater than 10";
 	}
+	return 0;
 }
