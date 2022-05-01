4c660279c47d8a0f45f77187e3e16a89329f0129
Korakoe <56580073+korakoe@users.noreply.github.com>
2022-05-01 15:53:15 +0800

    start the actual input stuff
    cin is bad for multi-word strings...

--- C++/Text Adventure.cpp
+++ C++/Text Adventure.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
 	cout << "-----|| This is a small text adventure game written in C++ ||----- \nThis is just so I can learn file saving, if/else statements and logic \n";
-	cout << "and just some general stuff, there will be a C port aswell";
+	cout << "and just some general stuff, there will be a C port aswell\n\n";
+
+	system("pause");
+
+	string name;
+
+	system("CLS");
+	cout << " Enter Your Name Adventurer! -- ";
+	getline(cin, name);
+
+	cout << "Hello, " << name << "\n\n";
+
+	system("pause");
 
 	return 0;
 }
