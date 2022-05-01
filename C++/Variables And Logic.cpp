1282616dce480ebf721ab356f38543f659d99d9a
Korakoe <56580073+korakoe@users.noreply.github.com>
2022-05-01 14:56:59 +0800

    CPP variables and logic

---
+++ C++/Variables And Logic.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// for the most part this is the same if not simpler than base C
+
+int main() {
+	int num1, num2;
+	cout << "Enter number one: ";
+	cin >> num1;
+	cout << "Enter number two: ";
+	cin >> num2;
+
+	int sum = num1 + num2;
+	cout << "Sum is " << to_string(sum) << "\n";
+
+	if (sum < 10) {
+		cout << "Less than 10";
+	}
+	else {
+		cout << "Greater than 10";
+	}
+}
